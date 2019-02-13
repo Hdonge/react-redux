201b7806595934780a5ce98ef15113cc2652c76a
Hdonge <harshal.donge@gmail.com>
2019-02-13 23:53:38 +0530

    Home, About Components and App layout

---
+++ myapp/src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This application uses react, redux, react router and other helpful libraries</p>
+            </div>
+        );
+    }
+}
+
+export default About;
