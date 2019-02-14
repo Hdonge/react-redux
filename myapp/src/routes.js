0bf6c08f55db6a418e85de9c427f2c49b82c0444
Hdonge <harshal.donge@gmail.com>
2019-02-14 14:16:02 +0530

    lints resolved

--- myapp/src/routes.js
+++ myapp/src/routes.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Home from './components/home/Homepage';
 import About from './components/about/AboutPage.js';
-import App from './components/App'
+import App from './components/App';
 
 
 export default (
-    <Route path='/' component={App}>
+    <Route path="/" component={App}>
         <IndexRoute component={Home} />
         <Route path="about" component={About} />
     </Route>
